a3fc79f9a419dc713a16d82f17e6e3807c3b138a
Binita <bbhusal394@gmail.com>
2026-04-05 09:02:49 +0545

    debug: enable DEBUG for troubleshooting

--- backend/civicaid_backend/settings.py
+++ backend/civicaid_backend/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '192.168.1.247', '.onrender.com', 'civicaid-backend-mwrq.onrender.com']
 
