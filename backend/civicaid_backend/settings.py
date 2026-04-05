4a159987bc15b970c395a4a7ef453eab450a0b85
Binita <bbhusal394@gmail.com>
2026-04-05 08:57:42 +0545

    fix: add render URL to ALLOWED_HOSTS

--- backend/civicaid_backend/settings.py
+++ backend/civicaid_backend/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '192.168.1.247', 'https://civicaid-backend-mwrq.onrender.com/']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '192.168.1.247', '.onrender.com', 'civicaid-backend-mwrq.onrender.com']
 
 
 # Application definition
