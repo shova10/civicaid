16f650d772febaae9bf91a5c145758c0603accd6
Binita <bbhusal394@gmail.com>
2026-04-24 22:45:18 +0545

    feat:added loggin in settings.py

--- backend/civicaid_backend/settings.py
+++ backend/civicaid_backend/settings.py
@@ -183,3 +183,17 @@ DATABASES = {
         ssl_require=True
     )
 }
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'DEBUG',
+    },
+}
