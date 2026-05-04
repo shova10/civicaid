b6b556636fcc75b87c6d2a7dbbb0bbeae241aaf5
Binita <bbhusal394@gmail.com>
2026-05-04 16:42:19 +0545

    change heatmap url

--- backend/complaints/urls.py
+++ backend/complaints/urls.py
@@ -3,11 +3,11 @@ from .views import ComplaintListCreateView, ComplaintDetailView, UpvoteToggleVie
 
 urlpatterns = [
     path('complaints/', ComplaintListCreateView.as_view(), name ='complaint-list'),
+    path('complaints/heatmap/', HeatmapView.as_view(), name='heatmap'),
     path('complaints/<int:pk>/', ComplaintDetailView.as_view(), name='complaint-detail'),
     path('complaints/<int:pk>/upvote/', UpvoteToggleView.as_view(), name='upvote-toggle'),
     path('admin/complaints/', AdminComplaintListView.as_view(), name='admin-complaint-list'),
     path('admin/complaints/<int:pk>/status/', StatusUpdateView.as_view(), name='status-update'),
-    path('complaints/heatmap/', HeatmapView.as_view(), name='heatmap'),
     path('admin/summary/', AdminSummaryView.as_view(), name='admin-summary'),
     path('admin/trends/', AdminTrendsView.as_view(), name='admin-trends'),
     path('admin/complaints/bulk-update/', BulkStatusUpdateView.as_view(), name='bulk-update'),
